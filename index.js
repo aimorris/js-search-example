16bb0a7df35b98b53b3df7229767668683e6c11d
Adam Morris <aimorris@aimorris.com>
2020-08-15 20:24:37 +1000

    done

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
-import * as JsSearch from 'js-search';
-
-let fileA = {
-  tags: ["RPC", "homework", "Effect of Controls", "pdf"],
-  link: "https://flightscope.gitlab.io/training/student-homework-sheets/effect-of-controls.pdf"
+function filterIt(arr, searchKey) {
+  return arr.filter(function(obj) {
+    return Object.keys(obj).some(function(key) {
+      return obj[key].includes(searchKey);
+    })
+  });
 }
 
-let fileB = {
-  tags: ["RPC", "homework", "Straight and Level", "pdf"],
-  link: "https://flightscope.gitlab.io/training/student-homework-sheets/straight-and-level.pdf"
-}
-
-let fileC = {
-  tags: ["RAAus", "Operations", "Manual", "pdf"],
-  link: "https://flightscope.gitlab.io/raaus-documentation/5-om-71-august-2016-single-pages.pdf"
-}
-
-let fileD = {
-  tags: ["Eurofox", "3K", "Flight Manual", "4844", "pdf"],
-  link: "https://flightscope.gitlab.io/aircraft/manual/eurofox-3k/24-4844/eurofox-3k_24-4844.pdf"
-}
-
-let fileE = {
-  tags: ["RPC", "homework", "Navigation", "html"],
-  link: "https://flightscope.gitlab.io/training/student-homework-sheets/nav-preparation.html"
-}
-
-let search = new JsSearch.Search('link');
-
-search.addIndex('tags');
-
-search.addDocuments([fileA, fileB, fileC, fileD, fileE]);
-
+let files = [
+  {
+    tags: ["RPC", "homework", "Effect of Controls", "pdf"],
+    link: "https://flightscope.gitlab.io/training/student-homework-sheets/effect-of-controls.pdf"
+  },
+  {
+    tags: ["RPC", "homework", "Straight and Level", "pdf"],
+    link: "https://flightscope.gitlab.io/training/student-homework-sheets/straight-and-level.pdf"
+  },
+  {
+    tags: ["RAAus", "Operations", "Manual", "pdf"],
+    link: "https://flightscope.gitlab.io/raaus-documentation/5-om-71-august-2016-single-pages.pdf"
+  },
+  {
+    tags: ["Eurofox", "3K", "Flight Manual", "4844", "pdf"],
+    link: "https://flightscope.gitlab.io/aircraft/manual/eurofox-3k/24-4844/eurofox-3k_24-4844.pdf"
+  },
+  {
+    tags: ["RPC", "homework", "Navigation", "html"],
+    link: "https://flightscope.gitlab.io/training/student-homework-sheets/nav-preparation.html"
+  }
+]
 let input = document.getElementById("input");
 let output = document.getElementById("results");
 
 input.addEventListener("keyup", function(e) {
   if (e.keyCode == 13) {
-    let results = search.search(input.value);
-    console.log(results);
-
-    output.innerHTML = '';
-
-    for (let i = 0; i < results.length; i++) {
-      output.innerHTML += results[i]["link"];
-      output.innerHTML += "<br>";
+    let results = filterIt(files, input.value);
+    output.innerHTML = "";
+    
+    for (obj of results) {
+      output.innerHTML += obj["link"];
+      output.innerHTML += "<br>"
     }
   }
 });
